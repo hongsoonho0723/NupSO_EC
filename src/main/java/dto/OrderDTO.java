62d3e2deaf70f21841ab8716b1b653bbaba923ff
hongsoonho0723 <ghstns357@gmail.com>
2024-04-26 17:06:35 +0900

    orderDTO수정

--- src/main/java/dto/OrderDTO.java
+++ src/main/java/dto/OrderDTO.java
@@ -9,6 +9,13 @@ public class OrderDTO {
 	private int orderState;
 	private String deliveryDate;
 	private String regDate;
+	private String deliveryAddr1;
+	private String deliveryAddr2;
+	private String deliveryName;
+	private String deliveryPhone;
+	private String deliveryMemo;
+	
+	
 	
 	//상세정보 가구이름 뺄때 사용하는 변수
 	OrderInfoDTO orderInfo = new OrderInfoDTO();
@@ -102,6 +109,49 @@ public class OrderDTO {
 	public void setRegDate(String regDate) {
 		this.regDate = regDate;
 	}
+
+	public String getDeliveryAddr1() {
+		return deliveryAddr1;
+	}
+
+	public void setDeliveryAddr1(String deliveryAddr1) {
+		this.deliveryAddr1 = deliveryAddr1;
+	}
+
+	public String getDeliveryAddr2() {
+		return deliveryAddr2;
+	}
+
+	public void setDeliveryAddr2(String deliveryAddr2) {
+		this.deliveryAddr2 = deliveryAddr2;
+	}
+
+	public String getDeliveryName() {
+		return deliveryName;
+	}
+
+	public void setDeliveryName(String deliveryName) {
+		this.deliveryName = deliveryName;
+	}
+
+	public String getDeliveryPhone() {
+		return deliveryPhone;
+	}
+
+	public void setDeliveryPhone(String deliveryPhone) {
+		this.deliveryPhone = deliveryPhone;
+	}
+
+	public String getDeliveryMemo() {
+		return deliveryMemo;
+	}
+
+	public void setDeliveryMemo(String deliveryMemo) {
+		this.deliveryMemo = deliveryMemo;
+	}
+	
+	
+	
 	
 	
 	
